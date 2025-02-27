25a4a2c571ce0b2bc1c3e02e6d30e0d5566dd813
tello070 <mijno.nl@gmail.com>
2025-02-27 14:35:23 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,12 @@
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'Driving Theory Test',
+  description: 'Practice for your driving theory test',
+}
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +14,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={inter.className}>{children}</body>
     </html>
   )
 }
